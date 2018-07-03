d01ecd1563955e6fae67f239d5012f2b120fd28e
hanwjdgh <hanwjdgh@naver.com>
2018-07-04 08:29:42 +0900

    Seven Dwarfs

---
+++ Brute-force Search/2309.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int arr[9];
+	int sum = 0, t_a = 0, t_b = 0;
+
+	for (int i = 0; i < 9; i++)
+		scanf("%d", &arr[i]);
+	sort(arr, arr + 9);
+
+	for (int i = 0; i < 9; i++)
+		sum += arr[i];
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (sum - (arr[i] + arr[j]) == 100) {
+				t_a = i;
+				t_b = j;
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < 9; i++) {
+		if (i == t_a || i == t_b)
+			continue;
+		else
+			printf("%d\n", arr[i]);
+	}
+	return 0;
+}
