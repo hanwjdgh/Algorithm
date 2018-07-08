0057a9aaa196541bcaa8ff4ca7421bbade2f20ec
hanwjdgh <hanwjdgh@naver.com>
2018-07-08 15:24:00 +0900

    Camping

---
+++ Greedy/4796.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+
+int main() {
+	int L, P, V;
+	int val,cnt=1;
+	while (1) {
+		scanf("%d %d %d", &L, &P, &V);
+		if (!L && !P && !V)
+			break;
+
+		if((V%P)<L)
+			printf("Case %d: %d\n",cnt++, (V / P)*L + (V%P));
+		else
+			printf("Case %d: %d\n", cnt++, (V / P)*L + L);
+	}
+	return 0;
+}
