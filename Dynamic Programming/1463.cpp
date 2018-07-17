912313cb0763a1f9f494fc0ca809e64d1f0e368a
hanwjdgh <hanwjdgh@naver.com>
2018-07-17 14:09:41 +0900

    Make it 1

--- Dynamic Programming/1463.cpp
+++ Dynamic Programming/1463.cpp
@@ -5,24 +5,24 @@
 
 using namespace std;
 
-vector <int > arr;
+vector <int > dp;
 
 int main() {
 	int N,num;
 
 	cin >> N;
 	
-	arr.push_back(0);
-	arr.push_back(0);
+	dp.push_back(0);
+	dp.push_back(0);
 
 	for (int i = 2; i <= N; i++) {
-		arr.push_back(arr[i - 1] + 1);
+		dp.push_back(dp[i - 1] + 1);
 		if (i % 3 == 0)
-			arr[i] = min(arr[i], arr[i / 3] + 1);
+			dp[i] = min(dp[i], dp[i / 3] + 1);
 		if(i%2 == 0)
-			arr[i] = min(arr[i], arr[i / 2] + 1);
+			dp[i] = min(dp[i], dp[i / 2] + 1);
 	}
 
-	cout << arr[N];
+	cout << dp[N];
 	return 0;
 }
