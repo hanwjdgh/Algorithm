0134723c415635a7fb56b36b2a2babc009165f6a
hanwjdgh <hanwjdgh@naver.com>
2019-01-28 15:06:21 +0900

    A

--- Codeforces/Educational  Round 59 (Div. 2)/A.cpp
+++ Codeforces/Educational  Round 59 (Div. 2)/A.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <ios>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
 
-    return 0;
+	int q;
+
+	cin >> q;
+
+	for (int i = 0; i < q; i++) {
+		int n;
+		string str;
+
+		cin >> n >> str;
+
+		if (n == 1 || (n == 2 && str[0] >= str[1]))
+			cout << "NO" << "\n";
+
+		else {
+			cout << "YES\n2" << "\n";
+			cout << str[0] << " " << str.substr(1) << "\n";
+		}
+
+	}
+
+	return 0;
 }
