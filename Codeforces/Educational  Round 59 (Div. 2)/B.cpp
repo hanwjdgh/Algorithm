6d91d90c30185d415abcf3937dada603f665222a
hanwjdgh <hanwjdgh@naver.com>
2019-01-28 15:17:34 +0900

    B

--- Codeforces/Educational  Round 59 (Div. 2)/B.cpp
+++ Codeforces/Educational  Round 59 (Div. 2)/B.cpp
@@ -4,9 +4,21 @@
 using namespace std;
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
 
-    return 0;
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		long long x, k;
+
+		cin >> x >> k;
+		
+		cout << 9 * (x - 1) + k << "\n";
+	}
+
+	return 0;
 }
