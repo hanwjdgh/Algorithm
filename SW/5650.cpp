15135a97d7a126432ff65d1d012d1b8f621d220c
hanwjdgh <hanwjdgh@naver.com>
2019-02-11 10:18:18 +0900

    5650

--- SW/5650.cpp
+++ SW/5650.cpp
@@ -14,32 +14,6 @@ int N, max_v, board[MAX][MAX];
 int block[6][4] = { { 0,0,0,0 },{ 1,3,0,2 },{ 3,2,0,1 },{ 2,0,3,1 },{ 2,3,1,0 },{ 2,3,0,1 } };
 int dir[4][2] = { { 0,-1 },{ -1,0 },{ 0,1 },{ 1,0 } };
 
-int move(int x, int y, int d) {
-	int ret = 0;
-	int nx = x + dir[d][0], ny = y + dir[d][1];
-
-	if (nx < 0 || ny < 0 || nx >= N || ny >= N)
-		ret = move(nx, ny, (d + 2) % 4) + 1;
-
-	else {
-		int num = board[nx][ny];
-
-		if (num == -1 || num == -2)
-			return 0;
-		else if (num == 0)
-			ret = move(nx, ny, d);
-		else if (num >= 1 && num <= 5)
-			ret = move(nx, ny, block[num][d]) + 1;
-		else {
-			if (v[num - 6][0].first == nx && v[num - 6][0].second == ny)
-				ret = move(v[num - 6][1].first, v[num - 6][1].second, d);
-			else
-				ret = move(v[num - 6][0].first, v[num - 6][0].second, d);
-		}
-	}
-	return ret;
-}
-
 int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
