d9ef81bfebdcb933bbd682fff7c25c27587e5293
hanwjdgh <hanwjdgh@naver.com>
2019-03-30 15:04:15 +0900

    5648

--- SW/5648.cpp
+++ SW/5648.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <ios>
+#include <queue>
 #include <cstring>
-#include <algorithm>
-
-#define MAX 4001
 
 using namespace std;
 
+struct Atom {
+	int y, x, d, e;
+};
+
+Atom atom[1001];
 int dir[4][2] = { {1,0},{-1,0},{0,-1},{0,1} };
-int N, board[MAX][MAX];
-int atom[1001][4];
+int N, board[4001][4001];
 
 int main() {
 	cin.tie(NULL);
@@ -22,56 +24,43 @@ int main() {
 
 	for (int t_case = 1; t_case <= T; t_case++) {
 		memset(board, 0, sizeof(board));
-		int x, y, d, e;
+		queue <Atom > q;
 		int cnt = 0;
 
 		cin >> N;
 
 		for (int i = 0; i < N; i++) {
-			cin >> x >> y >> d >> e;
-			atom[i][0] = (y + 1000) * 2;
-			atom[i][1] = (x + 1000) * 2;
-			atom[i][2] = d;
-			atom[i][3] = e;
-			board[(y + 1000) * 2][(x + 1000) * 2] = e;
+			cin >> atom[i].x >> atom[i].y >> atom[i].d >> atom[i].e;
+			atom[i].y = (atom[i].y + 1000) * 2;
+			atom[i].x = (atom[i].x + 1000) * 2;
+			board[atom[i].y][atom[i].x] = atom[i].e;
+			q.push(atom[i]);
 		}
+		while (!q.empty()) {
+			Atom cur_atom = q.front();
+			q.pop();
 
-		for (int i = 0; i <= MAX; i++) {
-			for (int j = 0; j < N; j++) {
-				int cx = atom[j][0], cy = atom[j][1], cd = atom[j][2], ce = atom[j][3];
+			if (board[cur_atom.y][cur_atom.x] > cur_atom.e) {
+				cnt += board[cur_atom.y][cur_atom.x];
+				board[cur_atom.y][cur_atom.x] = 0;
+				continue;
+			}
 
-				if (board[cx][cy] > ce) {
-					cnt += board[cx][cy];
-					board[cx][cy] = 0;
-					swap(atom[j], atom[N - 1]);
-					N--;
-					j--;
-					continue;
-				}
+			Atom next_atom = cur_atom;
+			next_atom.y += dir[next_atom.d][0], next_atom.x += dir[next_atom.d][1];
 
-				int nx = cx + dir[cd][0], ny = cy + dir[cd][1];
+			if (next_atom.y < 0 || next_atom.x < 0 || next_atom.y >= 4001 || next_atom.x >= 4001) {
+				board[cur_atom.y][cur_atom.x] = 0;
+				continue;
+			}
 
-				if (nx < 0 || ny < 0 || nx >= MAX || ny >= MAX) {
-					board[cx][cy] = 0;
-					swap(atom[j], atom[N - 1]);
-					N--;
-					j--;
-					continue;
-				}
-				atom[j][0] = nx, atom[j][1] = ny;
-
-				if (board[nx][ny] == 0) {
-					board[cx][cy] = 0;
-					board[nx][ny] = ce;
-				}
-				else {
-					board[cx][cy] = 0;
-					board[nx][ny] += ce;
-					swap(atom[j], atom[N - 1]);
-					N--;
-					j--;
-				}
-
+			board[cur_atom.y][cur_atom.x] = 0;
+			if (board[next_atom.y][next_atom.x] == 0) {
+				board[next_atom.y][next_atom.x] = next_atom.e;
+				q.push(next_atom);
+			}
+			else {
+				board[next_atom.y][next_atom.x] += next_atom.e;
 			}
 		}
 
