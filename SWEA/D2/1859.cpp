10910831cafbb9fe6a2ba8b58e30e1138b8a3786
hanwjdgh <hanwjdgh@naver.com>
2019-06-04 15:54:51 +0900

    1859

---
+++ SWEA/D2/1859.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <ios>
+
+using namespace std;
+
+int arr[1000001];
+
+int main(void){
+    cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n_case,t_case;
+    int max;
+    long long sum;
+  	
+    cin >> n_case;
+    
+    for(int i=1; i<=n_case;i++){
+    	cin >> t_case;
+        for(int j=0; j<t_case; j++)
+        	cin >> arr[j];
+        max=arr[t_case-1];
+        sum=0;
+        for(int j=t_case-2; j>-1; j--){
+           	if(max < arr[j])
+                max = arr[j];
+            else
+                sum += max -arr[j];
+        }
+        if (sum < 0)
+			sum = 0;
+        cout << "#"<<i<<" "<<sum<<endl;
+    }
+
+    return 0;
+}
